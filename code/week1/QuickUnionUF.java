12ce4b2f31e964e309ef82856db4f9d3fac8d5ba
JasonJ2021 <616445303@qq.com>
2021-01-23 16:55:55 +0800

    QuickUnionUF

---
+++ code/week1/QuickUnionUF.java
@@ -0,0 +1,26 @@
+package week1;
+
+public class QuickUnionUF {
+    private int[] id;
+    public QuickUnionUF(int num){
+        id = new int[num];
+        for (int i = 0; i < id.length; i++) {
+            id[i] = i;
+        }
+    }
+    public int findroot(int i){
+        while(id[i] != i){
+            i = id[i];
+        }
+        return i;
+    }
+    public boolean isConnected(int p , int q){
+      return findroot(p) == findroot(q);
+    }
+    public void union(int p , int q ){
+        int proot = findroot(p);
+        int qroot = findroot(q);
+        id[proot] = qroot;
+    }
+
+}
