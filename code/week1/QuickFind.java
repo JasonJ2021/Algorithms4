3573b95f8392c49a720d872343b145bf8b363665
JasonJ2021 <616445303@qq.com>
2021-01-23 16:34:35 +0800

    quickfind

---
+++ code/week1/QuickFind.java
@@ -0,0 +1,25 @@
+package week1;
+
+public class QuickFind {
+    private int[] id;
+    public QuickFind(int nums){
+        id = new int[nums];
+        for(int i = 0; i < id.length ; i++){
+            id[i] = i;
+        }
+    }
+    public boolean isConnected(int p , int q ){
+        return (id[p] == id[q]);
+    }
+
+    public void union(int p , int q ){
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if(id[i] == pid){
+                id[i] = qid;
+            }
+        }
+    }
+
+}
